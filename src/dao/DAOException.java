ae23ca41c50f6f972ee1df836f30893476e70eb4
Guscha Anna <ann9510.as@gmail.com>
2016-01-28 17:56:52 +0300

    rename url-patterns

---
+++ src/dao/DAOException.java
@@ -0,0 +1,17 @@
+package dao;
+
+/**
+ * Created by Anna on 1/28/2016.
+ */
+public class DAOException extends Exception {
+    public DAOException() {
+    }
+
+    public DAOException(String message) {
+        super(message);
+    }
+
+    public DAOException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
